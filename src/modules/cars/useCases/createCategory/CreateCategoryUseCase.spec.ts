559a38e324813444603f9225fee626addb9279c3
claudiobelempb <claudio.c.lima@hotmail.com>
2021-04-16 05:51:58 -0300

    Criando o primeiro teste

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,16 @@
+
+
+describe("Criar categoria", () => {
+  it("Espero que 2 + 2 = 4", () => {
+    const soma = 2 + 2;
+    const result = 4;
+
+    expect(soma).toBe(result);
+  });
+  it("Espero que 2 + 2 não seja 5", () => {
+    const soma = 2 + 2;
+    const result = 5;
+
+    expect(soma).not.toBe(result);
+  })
+});
